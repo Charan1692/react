cc64d686ff3f3cfc0c407c9ce1814135ecd7ea58
Vikas Sabaji <90546232+sabajiVikas@users.noreply.github.com>
2021-10-07 12:00:25 +0530

    Update config.js

--- realtime-contact/src/utils/config.js
+++ realtime-contact/src/utils/config.js
@@ -1,13 +1,4 @@
 //TODO: DONE add firebase configuration and export it
-export const firebaseConfig = {
-  apiKey: "AIzaSyAC45RkPvg5GBag7u8Uek96DT1KM_I__1U",
-  authDomain: "react-70ebc.firebaseapp.com",
-  databaseURL: "https://react-70ebc-default-rtdb.firebaseio.com",
-  projectId: "react-70ebc",
-  storageBucket: "react-70ebc.appspot.com",
-  messagingSenderId: "157667124382",
-  appId: "1:157667124382:web:84a1d6ed2e3f380444078f",
-};
 
 //image configuration
 export const imageConfig = {
